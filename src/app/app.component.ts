8dc62fd14a19809e1afcdbec4c5a445d248dd14a
lamnt95 <tunglam20132208@gmail.com>
2021-12-30 08:11:24 +0700

    2 files modified, 13 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { data } from './data/data.ts';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
+  constructor() {
+    console.log('data', data);
+  }
 }
---
+++ src/app/data/data.ts
@@ -0,0 +1,27 @@
+import { avax } from './avax.ts';
+import { bsc } from './bsc.ts';
+import { dot } from './dot.ts';
+import { ftm } from './ftm.ts';
+import { kn } from './kn.ts';
+import { nangcao } from './nangcao.ts';
+import { near } from './near.ts';
+import { other } from './other.ts';
+import { polygon } from './polygon.ts';
+import { sol } from './sol.ts';
+import { eth } from './eth.ts';
+import { cosmos } from './cosmos.ts';
+
+export const data = {
+  sol,
+  bsc,
+  avax,
+  eth,
+  near,
+  ftm,
+  polygon,
+  cosmos,
+  dot,
+  other,
+  kn,
+  nangcao,
+};
